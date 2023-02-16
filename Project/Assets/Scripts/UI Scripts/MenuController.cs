ae430a778ae000c9d6ecaad306ee720b20158bd9
TheDarkArtz <hfreisenbruch@gmail.com>
2023-02-16 17:58:26 +0000

    Squashed commit of the following:
    commit 8a278c68595913db77751e4ac54682d7611e5e7c
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Thu Feb 16 11:36:38 2023 +0000
    
        Bug fixing
    
        trying to fix scaling issues with menu elements
    
    commit d053fa02270d8d63779d29323e3ac314ca5797df
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Thu Feb 16 03:44:10 2023 +0000
    
        Menu Transitions done.
    
        Still working on code.......
    
    commit f7ab4fcb040ccffa28d5b3296d6398289f90dbe7
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Thu Feb 16 01:26:14 2023 +0000
    
        Character selector  layout done.
    
        Code is currently Semi done, transition between menus is pending.
    
    commit 3e2261d771aac555c57cc7ee26c70df6c437d854
    Merge: db4a103 6e64428
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Wed Feb 15 18:33:07 2023 +0000
    
        Merge branch 'main' into Jack
    
    commit 6e64428f5f02e5ddab30c889567689d5734a7ce1
    Merge: d172b45 49dcdb9
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Wed Feb 15 18:28:02 2023 +0000
    
        Merge branch 'main' of https://github.com/TheDarkArtz/Game-Studios---Unity-Project
    
    commit db4a103eca20c8810b791f9decd34c16ddbca2a8
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Tue Feb 14 03:28:23 2023 +0000
    
        bit's and bobs
    
        Just a last clean up before bed
    
    commit 2a0ccdb77e79244a95493a32320d60a483a78446
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Tue Feb 14 00:56:02 2023 +0000
    
        Added GameManager fixed crossfade and added music fade in/out
    
    commit 465174e47bbb40f6cbf6ab64b2aebf409c3d569f
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Mon Feb 13 23:34:38 2023 +0000
    
        Basic UI Code and crossfade
    
        Implemented code to load next scene, quit game and crossfade between scenes. Added crossfade panel and animation to Main Menu Scene.
    
    commit fc4792c3f780f44e8f5fd068c471116473e51d65
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Mon Feb 13 20:07:18 2023 +0000
    
        UI Elements added + Menu Scene
    
        Created the basic UI frame work in the Menu Scene. Buttons are clickable and respond on mouse hover.
    
    commit d172b45acdb750517204a3b1065fdb1f018940e2
    Author: SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
    Date:   Mon Feb 13 15:35:31 2023 +0000
    
        Jack Init Commit

--- Project/Assets/Scripts/UI Scripts/MenuController.cs
+++ Project/Assets/Scripts/UI Scripts/MenuController.cs
@@ -9,33 +9,79 @@ public class MenuController : MonoBehaviour
     [SerializeField] private Animator crossFadeTransistion;
     [SerializeField] private float transistionTime = 2;
     [SerializeField] private Animator musicFadeTransistion;
-
     private AudioSource audioSource;
+
+    [Header("Menu Transitions")]
+    [SerializeField] private Animator camaraTransistion;
+    [SerializeField] private Animator menuTransistion;
+
+    [Header("")]
+    [SerializeField] private GameObject[] characters;
+    public int selectedCharacter = 0;
+
+    
 
     private void Awake() 
     {
+        // Displays the mouse, but locks it to the game screen.
         Cursor.lockState = CursorLockMode.Confined;
 
         audioSource = GetComponent<AudioSource>();
     }
 
-    public void PlayGame()
+    public void CharacterSelectMenu()
     {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        camaraTransistion.SetTrigger("StartChr");
+        menuTransistion.SetTrigger("GoToChr");
     }
+
+    public void BackToMainMenu()
+    {
+        camaraTransistion.SetTrigger("StartMenu");
+        menuTransistion.SetTrigger("GoToMenu");
+    }
+
+    //The quit game event. Called by the quit game button.
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Quit");
     }
 
+    public void NextCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+        characters[selectedCharacter].SetActive(true);
+    }
+
+    public void PreviousCharacter()
+    {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter --;
+        if (selectedCharacter < 0)
+        {
+            selectedCharacter += characters.Length;
+        }
+        characters[selectedCharacter].SetActive(true);
+    }
+
+    //The play game event. Called by the play game button.
+    public void PlayGame()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
     IEnumerator LoadLevel(int levelIndex)
     {
         crossFadeTransistion.SetTrigger("Start");
-        musicFadeTransistion.SetTrigger("Start");
+        musicFadeTransistion.SetTrigger("StartFade");
 
         yield return new WaitForSeconds(transistionTime);
 
         SceneManager.LoadScene(levelIndex);
     }
+    
+
+    
 }
