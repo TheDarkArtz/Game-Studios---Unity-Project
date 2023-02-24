8d22aed179e9f628c5a3be20168dc7eda01c5787
SleepyJack01 <102834546+SleepyJack01@users.noreply.github.com>
2023-02-24 02:05:49 +0000

    Reworked Scene Transitions

--- Project/Assets/Scripts/UI Scripts/EndGameMenu.cs
+++ Project/Assets/Scripts/UI Scripts/EndGameMenu.cs
@@ -5,18 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class EndGameMenu : MonoBehaviour
 {
+
+    [Header("Scene Crossfade Controls")]
+    [SerializeField] private Animator crossFadeTransistion;
+    [SerializeField] private float transistionTime = 2;
+    [SerializeField] private Animator musicFadeTransistion;
+    private AudioSource audioSource;
     private void Awake() 
     {
         Cursor.lockState = CursorLockMode.Confined;
     }
     public void PlayAgain()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex -1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
     }
 
     public void MainMenu()
     {
-        SceneManager.LoadScene(0);
+        StartCoroutine(LoadLevel(0));
     }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        crossFadeTransistion.SetTrigger("Start");
+        musicFadeTransistion.SetTrigger("StartFade");
+
+        yield return new WaitForSeconds(transistionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    
 }
